4ff98d82ef664acab25085de6dd792043f0047b9
Zhu Quan <zhuquan01@corp.netease.com>
2018-11-09 20:57:50 +0800

    add eslint

--- src/bin.js
+++ src/bin.js
@@ -1,17 +1,20 @@
 const commander = require('commander');
-const updateNotifier = require('updateNotifier');
+const updateNotifier = require('update-notifier');
 const pkg = require('../package.json');
-const {lint} = require('./lint')
+const { lint } = require('./lint');
 
 commander.version(pkg.version, '-v, --version');
 
 commander
-    .option(`--fix`, 'fixes linting errors for select rules (this may overwrite linted files)')
-    .action(options => {
-        console.log('options --- ', options)
-        lint();
-    });
+  .option('-c, --config [config]', 'tslint configuration file')
+  .option('-p, --project [project]', 'tsconfig.json file')
+  .option('--fix', 'fixes linting errors for select rules (this may overwrite linted files)')
+  .action((options) => {
+    console.log('options --- ', options);
+    const { fix, config, project } = options;
+    lint({ fix, config, project });
+  });
 
-commander.parse(process.argv)
+commander.parse(process.argv);
 
 updateNotifier({ pkg }).notify();
--- src/lint.js
+++ src/lint.js
@@ -1,138 +1,144 @@
 // The code are modified from vue-cli 3, https://github.com/vuejs/vue-cli/blob/dev/packages/%40vue/cli-plugin-typescript/lib/tslint.js
 
-const fs = require('fs')
-const path = require('path')
-const tslint = require('tslint')
-const ts = require('typescript')
-const globby = require('globby')
-const vueCompiler = require('vue-template-compiler')
-
-const rootPath = process.cwd()
+const fs = require('fs');
+const path = require('path');
+const tslint = require('tslint');
+const ts = require('typescript');
+const globby = require('globby');
+const vueCompiler = require('vue-template-compiler');
 
 function resolveFromRoot(filePath) {
-  return path.join(rootPath, filePath)
+  return path.join(process.cwd(), filePath);
 }
 
+const isVueFile = file => /\.vue(\.ts)?$/.test(file);
+
 const lint = (args = {}) => {
-  /* eslint-disable-next-line node/no-extraneous-require */
-  const isVueFile = file => /\.vue(\.ts)?$/.test(file)
+  const configPath = resolveFromRoot(args.config || 'tslint.json');
+  const projectPath = resolveFromRoot(args.project || 'tsconfig.json');
 
   const defaultOptions = {
     fix: false,
     formatter: 'codeFrame',
-  }
-  const options = { ...defaultOptions, ...args }
+  };
+  const { fix } = args;
+  const options = { ...defaultOptions, fix };
 
   // hack to make tslint --fix work for *.vue files:
   // we save the non-script parts to a cache right before
   // linting the file, and patch fs.writeFileSync to combine the fixed script
   // back with the non-script parts.
   // this works because (luckily) tslint lints synchronously.
-  const vueFileCache = new Map()
-  const writeFileSync = fs.writeFileSync
+  const vueFileCache = new Map();
+  const { writeFileSync } = fs;
 
   const patchWriteFile = () => {
-    fs.writeFileSync = (file, content, options) => {
+    fs.writeFileSync = (file, content, option) => {
       if (isVueFile(file)) {
-        const parts = vueFileCache.get(path.normalize(file))
+        const parts = vueFileCache.get(path.normalize(file));
         if (parts) {
-          const { before, after } = parts
-          content = `${before}\n${content.trim()}\n${after}`
+          const { before, after } = parts;
+          // eslint-disable-next-line no-param-reassign
+          content = `${before}\n${content.trim()}\n${after}`;
         }
       }
-      return writeFileSync(file, content, options)
-    }
-  }
+      return writeFileSync(file, content, option);
+    };
+  };
 
   const restoreWriteFile = () => {
-    fs.writeFileSync = writeFileSync
-  }
+    fs.writeFileSync = writeFileSync;
+  };
 
-  const parseTSFromVueFile = file => {
-    const content = fs.readFileSync(file, 'utf-8')
-    const { script } = vueCompiler.parseComponent(content, { pad: 'line' })
+  // eslint-disable-next-line consistent-return
+  const parseTSFromVueFile = (file) => {
+    const content = fs.readFileSync(file, 'utf-8');
+    const { script } = vueCompiler.parseComponent(content, { pad: 'line' });
     if (script && /^tsx?$/.test(script.lang)) {
       vueFileCache.set(file, {
         before: content.slice(0, script.start),
         after: content.slice(script.end),
-      })
-      return script.content
+      });
+      return script.content;
     }
-  }
+  };
 
-  const program = tslint.Linter.createProgram(resolveFromRoot('tsconfig.json'))
+  const program = tslint.Linter.createProgram(projectPath);
 
   // patch getSourceFile for *.vue files
   // so that it returns the <script> block only
-  const patchProgram = program => {
-    const getSourceFile = program.getSourceFile
-    program.getSourceFile = function(file, languageVersion, onError) {
+  // eslint-disable-next-line no-shadow
+  const patchProgram = (program) => {
+    const { getSourceFile } = program;
+    // eslint-disable-next-line no-param-reassign
+    program.getSourceFile = (file, languageVersion, onError) => {
       if (isVueFile(file)) {
-        const script = parseTSFromVueFile(file) || ''
-        return ts.createSourceFile(file, script, languageVersion, true)
-      } else {
-        return getSourceFile.call(this, file, languageVersion, onError)
+        const script = parseTSFromVueFile(file) || '';
+        return ts.createSourceFile(file, script, languageVersion, true);
       }
-    }
-  }
+      return getSourceFile.call(this, file, languageVersion, onError);
+    };
+  };
 
-  patchProgram(program)
+  patchProgram(program);
 
-  const linter = new tslint.Linter(options, program)
+  const linter = new tslint.Linter(options, program);
 
   // patch linter.updateProgram to ensure every program has correct getSourceFile
-  const updateProgram = linter.updateProgram
-  linter.updateProgram = function(...args) {
-    updateProgram.call(this, ...args)
-    patchProgram(this.program)
-  }
+  const { updateProgram } = linter;
+  // eslint-disable-next-line no-shadow
+  linter.updateProgram = (...args) => {
+    updateProgram.call(this, ...args);
+    patchProgram(this.program);
+  };
 
-  const config = tslint.Configuration.findConfiguration(api.resolve('tslint.json')).results
+  const config = tslint.Configuration.findConfiguration(configPath).results;
   // create a patched config that disables the blank lines rule,
   // so that we get correct line numbers in error reports for *.vue files.
-  const vueConfig = Object.assign(config)
-  const rules = (vueConfig.rules = new Map(vueConfig.rules))
-  const rule = rules.get('no-consecutive-blank-lines')
+  const vueConfig = Object.assign(config);
+  // eslint-disable-next-line no-multi-assign
+  const rules = (vueConfig.rules = new Map(vueConfig.rules));
+  const rule = rules.get('no-consecutive-blank-lines');
   rules.set(
     'no-consecutive-blank-lines',
     Object.assign({}, rule, {
       ruleSeverity: 'off',
-    })
-  )
+    }),
+  );
 
-  const lint = file => {
-    const filePath = api.resolve(file)
-    const isVue = isVueFile(file)
-    patchWriteFile()
+  const lintFile = (file) => {
+    const filePath = resolveFromRoot(file);
+    const isVue = isVueFile(file);
+    patchWriteFile();
     linter.lint(
       // append .ts so that tslint apply TS rules
       filePath,
       '',
       // use Vue config to ignore blank lines
-      isVue ? vueConfig : config
-    )
-    restoreWriteFile()
-  }
+      isVue ? vueConfig : config,
+    );
+    restoreWriteFile();
+  };
 
-  const files = ['src/**/*.ts', 'src/**/*.vue', 'src/**/*.tsx', 'tests/**/*.ts', 'tests/**/*.tsx']
+  const filesPattern = ['./**/*.ts', './**/*.vue', './**/*.tsx'];
 
-  return globby(files, { rootPath }).then(files => {
-    files.forEach(lint)
-    const result = linter.getResult()
+  return globby(filesPattern, { cwd: projectPath }).then((files) => {
+    files.forEach(lintFile);
+    const result = linter.getResult();
     if (result.output.trim()) {
-      process.stdout.write(result.output)
+      process.stdout.write(result.output);
     } else if (result.fixes.length) {
       // some formatters do not report fixes.
-      const f = new tslint.Formatters.ProseFormatter()
-      process.stdout.write(f.format(result.failures, result.fixes))
+      const f = new tslint.Formatters.ProseFormatter();
+      process.stdout.write(f.format(result.failures, result.fixes));
     } else if (!result.failures.length) {
-      console.log(`No lint errors found.\n`)
+      console.log('No lint errors found.\n');
     }
 
     if (result.failures.length && !args.force) {
-      process.exitCode = 1
+      process.exitCode = 1;
     }
-  })
-}
+  });
+};
 
-module.exports = { lint }
+module.exports = { lint };
